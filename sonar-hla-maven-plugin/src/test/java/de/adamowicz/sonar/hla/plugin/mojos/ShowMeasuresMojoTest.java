1f5007f329a2f53da87b9f9c1479b77cb495b6f6
Bernd Adamowicz <info@bernd-adamowicz.de>
2014-08-19 12:06:13 +0200

    - added Jacoco configuration

---
+++ sonar-hla-maven-plugin/src/test/java/de/adamowicz/sonar/hla/plugin/mojos/ShowMeasuresMojoTest.java
@@ -0,0 +1,57 @@
+/*  _______________________________________
+ * < Sonar High Level API                  >
+ * < Copyright 2014 Bernd Adamowicz        >
+ * < mailto:info AT bernd-adamowicz DOT de >
+ *  ---------------------------------------
+ *  \
+ *   \   \_\_    _/_/
+ *    \      \__/
+ *           (oo)\_______
+ *           (__)\       )\/\
+ *               ||----w |
+ *               ||     ||
+ *
+ * Sonar-HLA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.adamowicz.sonar.hla.plugin.mojos;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Test cases for {@link ShowMeasuresMojo}. Admittedly no real tests here.
+ * 
+ * Created Aug 19, 2014 11:06:32 AM by bernd
+ */
+public class ShowMeasuresMojoTest {
+
+    private ShowMeasuresMojo mojo = null;
+
+    @BeforeClass
+    public void beforeClass() {
+
+        mojo = new ShowMeasuresMojo();
+    }
+
+    /**
+     * Simply issue information by executing <i>execute()</i>. No exception -> test OK.
+     */
+    @Test
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        mojo.execute();
+    }
+}
